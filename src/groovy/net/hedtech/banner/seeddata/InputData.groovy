05c30b5c2019877a1a11647b4db2a42662e7611b
Margy Hockett <margy.hockett@sungardhe.com>
2013-01-27 10:23:27 -0500

    add new file for hosweb013 to test min hours

--- src/groovy/net/hedtech/banner/seeddata/InputData.groovy
+++ src/groovy/net/hedtech/banner/seeddata/InputData.groovy
@@ -127,6 +127,7 @@ public class InputData {
             'ssbgeneralstudent': ['/src/groovy/net/hedtech/banner/seeddata/Data/GeneralStudentDataNew.xml'],
             'blockregistration': ['/src/groovy/net/hedtech/banner/seeddata/Data/BlockRegistration.xml'],
             'ssbgeneralstudent2': ['/src/groovy/net/hedtech/banner/seeddata/Data/GeneralStudentDataNew2.xml'],
+            'ssbgeneralstudent3': ['/src/groovy/net/hedtech/banner/seeddata/Data/GeneralStudentDataNew3.xml'],
             'ssbgeneralstudentblock': ['/src/groovy/net/hedtech/banner/seeddata/Data/GeneralStudentDataBlock.xml'],
             'ssbgeneralstudentblockrule': ['/src/groovy/net/hedtech/banner/seeddata/Data/GeneralStudentDataBlockWithRule.xml'],
             'attr': ['src/groovy/net/hedtech/banner/seeddata/Data/AttendanceTrackingAdminRuleSetupSeed.xml',
