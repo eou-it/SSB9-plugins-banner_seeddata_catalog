a224b1f8d707f33500a4e874a7cb0d5662d4a6c3
Margy Hockett <margy.hockett@sungardhe.com>
2013-08-07 08:13:34 -0400

    update copyright statement

--- src/groovy/net/hedtech/banner/seeddata/scripts/find_test_data.sql
+++ src/groovy/net/hedtech/banner/seeddata/scripts/find_test_data.sql
@@ -1,17 +1,6 @@
 -- *****************************************************************************************
--- * Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.                    *
--- * This copyrighted software contains confidential and proprietary information of        *
--- * SunGard Higher Education and its subsidiaries. Any use of this software is limited    *
--- * solely to SunGard Higher Education licensees, and is further subject to the terms     *
--- * and conditions of one or more written license agreements between SunGard Higher       *
--- * Education and the licensee in question. SunGard is either a registered trademark or   *
--- * trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.*
--- * Banner and Luminis are either registered trademarks or trademarks of SunGard Higher   *
--- * Education in the U.S.A. and/or other regions and/or countries.                        *
+-- * Copyright 2010-2013 Ellucian Company L.P. and its affiliates.                         *
 -- *****************************************************************************************
-
-
-
 
 -- this script will generate a Select statement For the table entered And report records that are 
 -- horizon test data.  Those records are identified With GRAILS in the data origin, user id Or 01/01/2010
