f78cb86977bf0bf503d919fb59bcf8b5cd49f18a
Raji Venkatesan <rvenkate@.corp.sct.com>
2014-03-03 16:32:15 -0500

    Adding employee self service users

--- src/groovy/net/hedtech/banner/seeddata/InputData.groovy
+++ src/groovy/net/hedtech/banner/seeddata/InputData.groovy
@@ -166,7 +166,8 @@ public class InputData {
             'registration-withdrawal': ['/src/groovy/net/hedtech/banner/seeddata/Data/RegistrationHOSR24796.xml'],
             'registration-planning': ['/src/groovy/net/hedtech/banner/seeddata/Data/SelfServiceStudentRegistrationPlanSeed.xml']  ,
             'transcript-award' : ['/src/groovy/net/hedtech/banner/seeddata/Data/StudentTranscriptAwardedDegree.xml'],
-             'student-adv-admin-persona' :['/src/groovy/net/hedtech/banner/seeddata/Data/StudentAdvisorAdminPersonData.xml']
+             'student-adv-admin-persona' :['/src/groovy/net/hedtech/banner/seeddata/Data/StudentAdvisorAdminPersonData.xml'],
+             'employee' : ['/src/groovy/net/hedtech/banner/seeddata/Data/SelfServiceEmployees.xml']
     ]
 
     /**
