274bd4a9b178cd41a7f0a339d46df01de357d918
Margy Hockett <margy.hockett@ellucian.com>
2016-01-22 12:37:24 -0500

    Update copyright year

--- src/groovy/net/hedtech/banner/seeddata/InputData.groovy
+++ src/groovy/net/hedtech/banner/seeddata/InputData.groovy
@@ -1,5 +1,5 @@
 /*********************************************************************************
- Copyright 2010-2015 Ellucian Company L.P. and its affiliates.
+ Copyright 2010-2016 Ellucian Company L.P. and its affiliates.
  **********************************************************************************/
 package net.hedtech.banner.seeddata
 import grails.util.GrailsUtil
