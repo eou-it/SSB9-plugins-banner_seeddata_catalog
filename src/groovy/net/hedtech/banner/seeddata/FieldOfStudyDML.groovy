dc3793db1a846af65b805490256914036b254265
Margy Hockett <margy.hockett@ellucian.com>
2016-11-04 14:14:34 -0400

    remove update to sobctrl as its causing lock

--- src/groovy/net/hedtech/banner/seeddata/FieldOfStudyDML.groovy
+++ src/groovy/net/hedtech/banner/seeddata/FieldOfStudyDML.groovy
@@ -88,7 +88,7 @@ public class FieldOfStudyDML {
 
 
     def processSorlfos() {
-        conn.executeUpdate("update sobctrl set sobctrl_curr_rule_ind = 'Y'")
+       // conn.executeUpdate("update sobctrl set sobctrl_curr_rule_ind = 'Y'")
         tableRow = null
         String rowSQL = """select rowid table_row from SORLFOS
            where sorlfos_pidm = ? and sorlfos_lcur_seqno = ? and sorlfos_seqno = ? """
