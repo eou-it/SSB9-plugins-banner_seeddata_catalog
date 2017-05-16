95cbb23b19baddd123b408cf76c33f0823c210ee
lokeshn <lokesh.nagappa@ellucian.com>
2017-05-16 17:18:03 +0530

    Enhanced the plugin to work independently so that it can be automated.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,40 +1,12 @@
-/*********************************************************************************
-  Copyright 2010-2013 Ellucian Company L.P. and its affiliates.
- **********************************************************************************/
-
-// Support Hibernate annotations
+import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
 
 
-
-// Note: Most of the dataSource configuration resides in resources.groovy and in the
-// installation-specific configuration file (see Config.groovy for the include).
-
-dataSource {
-   
-    dialect = "org.hibernate.dialect.Oracle10gDialect"
-    loggingSql = false
-
-}
-
-
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-   	hbm2ddl.auto = null
-   	show_sql = false
-//   	naming_strategy = "org.hibernate.cfg.ImprovedNamingStrategy"
-   	dialect = "org.hibernate.dialect.Oracle10gDialect"
-    config.location = [
-            "classpath:hibernate-banner-core.cfg.xml"
-    ]
-}
-
-
+// environment specific settings
 // environment specific settings
 environments {
     development {
         dataSource {
+
         }
     }
     test {
