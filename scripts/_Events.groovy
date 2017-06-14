76bfbd24c356ea7436f30565840369588faf0bca
lokeshn <lokesh.nagappa@ellucian.com>
2017-06-14 13:07:54 +0530

    added GANT script code to exclude ojdbc6.jar during war packaging

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -5,9 +5,6 @@ import grails.util.Environment
 * Copyright 2010-2013 Ellucian Company L.P. and its affiliates.                         *
 *****************************************************************************************/
 
-
-
-
-
-
-
+eventCreateWarStart = { warName, stagingDir ->
+    ant.delete( file: "${stagingDir}/WEB-INF/lib/ojdbc6.jar" )
+}
