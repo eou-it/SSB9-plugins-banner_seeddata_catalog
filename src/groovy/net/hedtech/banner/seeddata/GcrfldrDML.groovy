efa7389a85a659108ee21235a55269139c5a230f
Tiwari Shiv <shiv.tiwari@ellucian.com>
2017-09-29 12:35:46 +0530

    Corrected TYPO in column name

--- src/groovy/net/hedtech/banner/seeddata/GcrfldrDML.groovy
+++ src/groovy/net/hedtech/banner/seeddata/GcrfldrDML.groovy
@@ -174,7 +174,7 @@ public class GcrfldrDML {
         deleteData("GCBTMPL", "delete from GCBTMPL where GCBTMPL_folder_id  = ?   ")
         deleteData("GCBQURY", "delete from GCBQURY where GCBQURY_folder_id  = ? ")
         deleteData("GCRCFLD", "delete from GCRCFLD where GCRCFLD_folder_id = ?  ")
-        deleteData("GCRFLDR", "delete from GCRFLDR where GCRFLDR_surrogate_id = ? and  NOT EXISTS (SELECT a.gcbactm_gcrafldr_id FROM gcbactm a WHERE a.gcbactm_gcrafldr_id = gcrfldr_surrogate_id) ")
+        deleteData("GCRFLDR", "delete from GCRFLDR where GCRFLDR_surrogate_id = ? and  NOT EXISTS (SELECT a.gcbactm_gcrfldr_id FROM gcbactm a WHERE a.gcbactm_gcrfldr_id = gcrfldr_surrogate_id) ")
 
     }
 
