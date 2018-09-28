f155a745e0c096628d8d4748901714a89473ebcb
anandka <anand.karadi@ellucian.com>
2018-09-28 16:56:14 +0530

    Seed data

--- src/groovy/net/hedtech/banner/seeddata/InputData.groovy
+++ src/groovy/net/hedtech/banner/seeddata/InputData.groovy
@@ -758,8 +758,10 @@ public class InputData {
             'generalstudenthosapi005'          : ['/src/groovy/net/hedtech/banner/seeddata/Data/GeneralStudentDataHosapi005.xml'],
             'jobsub'                           : ['/src/groovy/net/hedtech/banner/seeddata/Data/jobsub.xml'],
             'self-service-configuration'       : ['/src/groovy/net/hedtech/banner/seeddata/Data/SelfServiceConfiguration.xml'],
-            'reset-password':['/src/groovy/net/hedtech/banner/seeddata/Data/resetPasswordUserSetupData.xml',
-                              '/src/groovy/net/hedtech/banner/seeddata/Data/GuestUserSetupData.xml']
+            'reset-password'                   :['/src/groovy/net/hedtech/banner/seeddata/Data/resetPasswordUserSetupData.xml',
+                                                 '/src/groovy/net/hedtech/banner/seeddata/Data/GuestUserSetupData.xml'],
+            'ar-data'                          :['/src/groovy/net/hedtech/banner/seeddata/Data/ar/User_Data.xml',
+                                                 '/src/groovy/net/hedtech/banner/seeddata/Data/ar/TransactionData.xml']
     ]
     /**
      *  Map of selenium targets
