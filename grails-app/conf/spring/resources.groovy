08729212a8772aafc3564fa0e385bbe8b71c0ed1
Barun Kumar Patel <barun.patel@ellucian.com>
2019-05-30 14:24:48 +0530

    HRU-8758:- Removed unwanted code.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -11,11 +11,6 @@
  * Spring bean configuration using Groovy DSL, versus normal Spring XML.
  */
 beans = {
-    dataSource(org.apache.commons.dbcp.BasicDataSource) {
-        java.util.TimeZone defaultTimeZone = TimeZone.getTimeZone("America/New_York")
-        java.util.TimeZone.setDefault(defaultTimeZone)
-        println "\n \n configure spring/resources.groovy defaultTimeZone $defaultTimeZone"
-    }
-    
+
 }
 
