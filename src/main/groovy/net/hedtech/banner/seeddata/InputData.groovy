36c4f297f12cb24181807f7d467abb8caa7db238
Rosalinda Jaimes <rosalinda.jaimes@ellucian.com>
2020-06-26 19:42:59 -0500

    Correct the information for tests with MC ON.

--- src/main/groovy/net/hedtech/banner/seeddata/InputData.groovy
+++ src/main/groovy/net/hedtech/banner/seeddata/InputData.groovy
@@ -1134,14 +1134,7 @@ public class InputData {
                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/student/studentAid/ValidationTablesBsa.xml',
                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/student/studentAid/ValidationTablesNonBSA.xml',
                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/student/studentAid/StudentAidNonIDTables.xml',
-                                                    '/src/main/groovy/net/hedtech/banner/seeddata/Data/student/studentAid/StudentAidIDTables.xml'],
-            'multicurrencyConfiguration'          : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/CreateCurrencyCode.xml',
-                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/AllFor209999Term.xml',
-                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/FeeAssesmentDetailsRules.xml'],
-            'multicurrencyStudent000'             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0000.xml'],
-            'multicurrencyStudent001'             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0001.xml'],
-            'multicurrencyStudent002'             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0002.xml'],
-            'multicurrencyStudent003'             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0003.xml']
+                                                    '/src/main/groovy/net/hedtech/banner/seeddata/Data/student/studentAid/StudentAidIDTables.xml']
 
     ]
     /**
@@ -1333,7 +1326,14 @@ public class InputData {
                                                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/RegistrationMutualExclusionsHOSMEX009.xml',
                                                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/RegistrationMutualExclusionsHOSMEX010.xml'],
             'selfService-registration-nameDisplay'                                :['/src/main/groovy/net/hedtech/banner/seeddata/Data/SelfServiceRegistrationNameDisplay.xml'],
-            'selfService-registration-nameDisplay-reset'                          :['/src/main/groovy/net/hedtech/banner/seeddata/Data/SelfServiceRegistrationNameDisplayReset.xml']
+            'selfService-registration-nameDisplay-reset'                          :['/src/main/groovy/net/hedtech/banner/seeddata/Data/SelfServiceRegistrationNameDisplayReset.xml'],
+            'multicurrencyConfiguration'                                          : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/CreateCurrencyCode.xml',
+                                                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/FeeAssesmentDetailsRules.xml',
+                                                                                     '/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/AllFor209999Term.xml'],
+            'multicurrencyStudent000'                                             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0000.xml'],
+            'multicurrencyStudent001'                                             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0001.xml'],
+            'multicurrencyStudent002'                                             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0002.xml'],
+            'multicurrencyStudent003'                                             : ['/src/main/groovy/net/hedtech/banner/seeddata/Data/multicurrency/UserHOSMC0003.xml']
     ]
 
     def calbTargets = [
